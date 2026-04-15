38a46eabb29f3d4470a75d6f02dcbf4753cea122
Chandana Omkarappa <chandanaomkarappa@gmail.com>
2026-04-15 18:16:14 +0530

    methodsusingswitchcase examples

---
+++ methodsusingswitchcase/Contacts.java
@@ -0,0 +1,607 @@
+class Contacts
+{
+	static long search(String name){
+		long no=0L;
+		switch(name){
+			case "Drama Queen":
+			no = 6364212629L;
+			System.out.println("Case 1");
+			break;
+			case "Daddy":
+			no = 9980051095L;
+			System.out.println("Case 2");
+			break;
+			case "Mumma":
+			no =6364890106L;
+			System.out.println("Case 3");
+			break;
+			case "Srujana":
+			no = 7892182192L;
+			System.out.println("Case 4");
+			break;
+			case "Manju":
+			no =9008547568L;
+			System.out.println("Case 5");
+			break;
+			case "Akshatha":
+			no=9108260528L;
+			System.out.println("Case 6");
+			break;
+			case "Soumya":
+			no=6360438274L;
+			System.out.println("Case 7");
+			break;
+			case "Vyshnavi":
+			no=9980801478L;
+			System.out.println("Case 8");
+			break;
+			case "Hema":
+			no=7676420983L;
+			System.out.println("Case 9");
+			break;
+			case "Sandya":
+			no=9611588479L;
+			System.out.println("Case 10");
+			break;
+			case "Ganesh":
+			no= 8951536148L;
+			System.out.println("Case 11");
+			break;
+			case "Abhiram":
+			no=9483296548L;
+			System.out.println("Case 12");
+			break;
+			case "Yamini":
+			no=6364210640L;
+			System.out.println("Case 13");
+			break;
+			case "Spoorthi":
+			no= 9108204232L;
+			System.out.println("Case 14");
+			break;
+			case "Noor":
+			no=8050998870L;
+			System.out.println("Case 15");
+			break;
+			case "Varsha":
+			no=953585775L;
+			System.out.println("Case 16");
+			break;
+			case "Aiman":
+			no=9743146885L;
+			System.out.println("Case 17");
+			break;
+			case "Yashu":
+			no=6361980460L;
+			System.out.println("Case 18");
+			break;
+			case "Uzma":
+			no=8904220115L;
+			System.out.println("Case 19");
+			break;
+			case "Umera":
+			no=9380800425L;
+			System.out.println("Case 20");
+			break;
+			case "Umme Kulsum":
+			no=8747088978L;
+			System.out.println("Case 21");
+			break;
+			case "Teju":
+			no=9663780699L;
+			System.out.println("Case 22");
+			break;
+			case "Tejasvini":
+			no=9483360461L;
+			System.out.println("Case 23");
+			break;
+			case "Tejaswini Bsc":
+			no=9353491161L;
+			System.out.println("Case 24");
+			break;
+			case "Tasmiya Jnes":
+			no=9380054155L;
+			System.out.println("Case 25");
+			break;
+			case "Tasleem Jnes":
+			no=9663325736L;
+			System.out.println("Case 26");
+			break;
+			case "Tanuja Badima":
+			no=9980428152L;
+			System.out.println("Case 27");
+			break;case "Tanu Sav":
+			no=9113613271L;
+			System.out.println("Case 28");
+			break;case "Tanu":
+			no=9980428152L;
+			System.out.println("Case 29");
+			break;case "Sweety":
+			no=7204835031L;
+			System.out.println("Case 30");
+			break;
+			case "Sushmita":
+			no=9845451531L;
+			System.out.println("Case 31");
+			break;
+			case "Surjith":
+			no=8217827798L;
+			System.out.println("Case 32");
+			break;
+			case "Suresh":
+			no=9986502357L;
+			System.out.println("Case 33");
+			break;
+			case "Sunitha":
+			no=7676495404L;
+			System.out.println("Case 34");
+			break;
+			case "Sunitha Mami":
+			no=9108744691L;
+			System.out.println("Case 35");
+			break;
+			case "Summitra":
+			no=7795796174L;
+			System.out.println("Case 36");
+			break;
+			case "Summaya":
+			no=8217416902L;
+			System.out.println("Case 37");
+			break;
+			case "Sujan":
+			no=9481174624L;
+			System.out.println("Case 38");
+			break;
+			case "Suhana":
+			no=6364084711L;
+			System.out.println("Case 39");
+			break;
+			case "Sudheer":
+			no=9611128352L;
+			System.out.println("Case 40");
+			break;
+			case "Shreyas":
+			no=9880061026L;
+			System.out.println("Case 41");
+			break;
+			case "Spoorthi Dvs":
+			no=7338519184L;
+			System.out.println("Case 42");
+			break;
+			case "Spoo":
+			no=9972378014L;
+			System.out.println("Case 43");
+			break;
+			case "Soumya Sis":
+			no=6361162596L;
+			System.out.println("Case 44");
+			break;
+			case "Soumya Raj Dvs":
+			no=9945384935L;
+			System.out.println("Case 45");
+			break;
+			case "Soudamini":
+			no=7411407058L;
+			System.out.println("Case 46");
+			break;
+			case "Sonika Dvs":
+			no=6363741489L;
+			System.out.println("Case 47");
+			break;
+			case "Sonika":
+			no=9844281235L;
+			System.out.println("Case 48");
+			break;
+			case "Sneha":
+			no=8310857027L;
+			System.out.println("Case 49");
+			break;
+			case "Sneha DVS":
+			no=8277606800L;
+			System.out.println("Case 50");
+			break;
+			case "Sinchana S Dvs":
+			no=8660943289L;
+			System.out.println("Case 51");
+			break;
+			case "Sinchana R":
+			no=8618316689L;
+			System.out.println("Case 52");
+			break;
+			case "Sinchana Baggi Dvs":
+			no=9071840010L;
+			System.out.println("Case 53");
+			break;
+			case "Shwetha Akka Dvs":
+			no=7259872154L;
+			System.out.println("Case 54");
+			break;
+			case "Shubha":
+			no=9980451820L;
+			System.out.println("Case 55");
+			break;
+			case "Shruthi":
+			no=7019437293L;
+			System.out.println("Case 56");
+			break;
+			case "Shreyanka":
+			no=9110630018L;
+			System.out.println("Case 57");
+			break;
+			case "Shivram Mousa":
+			no=9845222898L;
+			System.out.println("Case 58");
+			break;
+			case "Shivani Dvs":
+			no=9019653896L;
+			System.out.println("Case 59");
+			break;
+			case "Sharath Sir Placement Cell":
+			no=9060909027L;
+			System.out.println("Case 60");
+			break;
+			case "Shalini Jnes":
+			no=8073697756L;
+			System.out.println("Case 61");
+			break;
+			case "Shalini L":
+			no=7349147908L;
+			System.out.println("Case 62");
+			break;
+			case "Shalini":
+			no=7760045206L;
+			System.out.println("Case 63");
+			break;
+			case "Shali L":
+			no=9110494163L;
+			System.out.println("Case 64");
+			break;
+			case "Shaila Bca":
+			no=7676092184L;
+			System.out.println("Case 65");
+			break;
+			case "Savithri maam":
+			no=9945154081L;
+			System.out.println("Case 66");
+			break;
+			case "Sarswathi Aunty":
+			no=9945557709L;
+			System.out.println("Case 67");
+			break;
+			case "Saouda":
+			no=9606423787L;
+			System.out.println("Case 68");
+			break;
+			case "Sanju":
+			no=6364768033L;
+			System.out.println("Case 69");
+			break;
+			case "Sanju Sonu Rajput":
+			no=9740174891L;
+			System.out.println("Case 70");
+			break;
+			case "Sanju Dvs":
+			no=7795300865L;
+			System.out.println("Case 71");
+			break;
+			case "Sanjay Dvs":
+			no=9900972761L;
+			System.out.println("Case 72");
+			break;
+			case "Sanjana SA":
+			no=7892031681L;
+			System.out.println("Case 73");
+			break;
+			case "Sanjana R Juniour":
+			no=8618693101L;
+			System.out.println("Case 74");
+			break;
+			case "Sanjana P":
+			no=9008060513L;
+			System.out.println("Case 75");
+			break;
+			case "Sanjana M Prasad":
+			no=9164904610L;
+			System.out.println("Case 76");
+			break;
+			case "Sanjana Kadkol Jnnce":
+			no=7204171032L;
+			System.out.println("Case 77");
+			break;
+			case "Sanjana K":
+			no=77905067306L;
+			System.out.println("Case 78");
+			break;
+			case "Sanjana ":
+			no=9353112171L;
+			System.out.println("Case 79");
+			break;
+			case "Saniya Tasleem":
+			no=9986526307L;
+			System.out.println("Case 80");
+			break;
+			case "Saniya Aiman":
+			no=7411878513L;
+			System.out.println("Case 81");
+			break;
+			case "Sangeetha":
+			no=7022899641L;
+			System.out.println("Case 82");
+			break;
+			case "Sandeep Mama Bdvt":
+			no=8095409817L;
+			System.out.println("Case 83");
+			break;
+			case "Samana":
+			no=8277044763L;
+			System.out.println("Case 84");
+			break;
+			case "Sakshi":
+			no=9740451593L;
+			System.out.println("Case 85");
+			break;
+			case "Sakshi Dvs":
+			no=8050595630L;
+			System.out.println("Case 86");
+			break;
+			case "Sahana Miss":
+			no=8867856709L;
+			System.out.println("Case 87");
+			break;
+			case "Sahana Mam Sav":
+			no=9380622268L;
+			System.out.println("Case 88");
+			break;
+			case "Sahana Jnnce":
+			no=8050931885L;
+			System.out.println("Case 89");
+			break;
+			case "Sadiya Jnnce":
+			no=7760497556L;
+			System.out.println("Case 90");
+			break;
+			case "Ruchitha Jnnce":
+			no=7022392944L;
+			System.out.println("Case 91");
+			break;
+			case "Roopa Senior Jnnce":
+			no=6362842374L;
+			System.out.println("Case 92");
+			break;
+			case "Roopa Badimaa":
+			no=9844970623L;
+			System.out.println("Case 93");
+			break;
+			case "Rekha Puppu":
+			no=9035752815L;
+			System.out.println("Case 94");
+			break;
+			case "Rekha":
+			no=9482153785L;
+			System.out.println("Case 95");
+			break;
+			case "Raziya Maam":
+			no=9108364432L;
+			System.out.println("Case 96");
+			break;
+			case "Ratna Sonu":
+			no=8217601875L;
+			System.out.println("Case 97");
+			break;
+			case "Rasmitha":
+			no=9606230067L;
+			System.out.println("98");
+			break;
+			case "Rashmi Maam Dvs":
+			no=9535461223L;
+			System.out.println("Case 99");
+			break;
+			case "Rashi Beauty Parlour":
+			no=9880038969L;
+			System.out.println("Case 100");
+			break;
+			case "Ranjitha Jnnce":
+			no=6360179043L;
+			System.out.println("Case 101");
+			break;
+			case "Ranjitha Akka":
+			no=9611892923L;
+			System.out.println("Case 102");
+			break;
+			case "Ramya Sonu Sav":
+			no=9743346064L;
+			System.out.println("Case 103");
+			break;
+			case "Ramya Jnnce":
+			no=9900820828L;
+			System.out.println("Case 104");
+			break;
+			case "Rakshitha Mam":
+			no=9738865181L;
+			System.out.println("Case 105");
+			break;
+			case "Rakshitha Jnnce":
+			no=7337600732L;
+			System.out.println("Case 106");
+			break;
+			case "Raja Fashions":
+			no=9880086565L;
+			System.out.println("Case 107");
+			break;
+			case "Rahul NK jnnce":
+			no=7975719907L;
+			System.out.println("Case 108");
+			break;
+			case "Rahul NK Dvs":
+			no=8088163662L;
+			System.out.println("Case 109");
+			break;
+			case "Raghu Mama":
+			no=9482207180L;
+			System.out.println("Case 110");
+			break;
+			case "Raghavi":
+			no=8073957626L;
+			System.out.println("Case 111");
+			break;
+			case "Ragvendra Sir Jnnce":
+			no=7019095137L;
+			System.out.println("Case 112");
+			break;case "Ragvendra Dvs":
+			no=8867144372L;
+			System.out.println("Case 113");
+			break;
+			case "Punith Kademane":
+			no=9535167488L;
+			System.out.println("Case 114");
+			break;
+			case "Punith Dvs":
+			no=9146446966L;
+			System.out.println("Case 115");
+			break;
+			case "Priyanka Maam":
+			no=7204376031L;
+			System.out.println("Case 116");
+			break;
+			case "Priya Sonu":
+			no=8971796946L;
+			System.out.println("Case 117");
+			break;
+			case "Priya Dvs":
+			no=6364771445L;
+			System.out.println("Case 118");
+			break;
+			case "Prarthana Jnnce":
+			no=8147557161L;
+			System.out.println("Case 119");
+			break;
+			case "Prarthana":
+			no=9740968993L;
+			System.out.println("Case 120");
+			break;
+			case "Prajwal Singh Bro":
+			no=9480206877L;
+			System.out.println("Case 121");
+			break;
+			case "Prajwal Bhaya":
+			no=6364352057L;
+			System.out.println("Case 122");
+			break;
+			case "Pragathi Jnnce":
+			no=6364781971L;
+			System.out.println("Case 123");
+			break;
+			case "Prabha Bhatt Mam":
+			no=9036093038L;
+			System.out.println("Case 124");
+			break;
+			case "Powrnami Sonu frnd":
+			no=7618757542L;
+			System.out.println("Case 125");
+			break;
+			case "Poorvitha":
+			no=6362400256L;
+			System.out.println("Case 126");
+			break;
+			case "Poorvi":
+			no=9632989439L;
+			System.out.println("Case 127");
+			break;
+			case "Pooja Jnnce":
+			no=9686599159L;
+			System.out.println("Case 128");
+			break;
+			case "Pooja":
+			no=7259041363L;
+			System.out.println("Case 129");
+			break;
+			case "Pavan":
+			no=6360015825L;
+			System.out.println("Case 130");
+			break;
+			case "Pari":
+			no=7019639980L;
+			System.out.println("Case 131");
+			break;
+			case "Pappa":
+			no=7483574575L;
+			System.out.println("Case 132");
+			break;
+			case "Dad":
+			no=9902281627L;
+			System.out.println("Case 133");
+			break;
+			case "Panendra Sir":
+			no=8867261635L;
+			System.out.println("Case 134");
+			break;
+			case "Padma Aunty":
+			no=9632075069L;
+			System.out.println("Case 135");
+			break;
+			case "Noothan":
+			no=8073935287L;
+			System.out.println("Case 136");
+			break;
+			case "Noorain Bro":
+			no=9148339088L;
+			System.out.println("Case 137");
+			break;
+			case "Noorain":
+			no=9535385050L;
+			System.out.println("Case 138");
+			break;
+			case "Noor Mom":
+			no=6364736563L;
+			System.out.println("Case 139");
+			break;
+			case "Niveditha Dvs":
+			no=7795142663L;
+			System.out.println("Case 140");
+			break;
+			case "Nirmala Mousi":
+			no=9880250476L;
+			System.out.println("Case 141");
+			break;
+			case "Nikkath Maam":
+			no=9632727738L;
+			System.out.println("Case 142");
+			break;
+			case "Nikitha Mam":
+			no=9741294819L;
+			System.out.println("Case 143");
+			break;
+			case "Nikitha":
+			no=8310758972L;
+			System.out.println("Case 144");
+			break;
+			case "Nethravathi Mam":
+			no=9241223948L;
+			System.out.println("Case 145");
+			break;
+			case "Neha":
+			no=9916266836L;
+			System.out.println("Case 146");
+			break;
+			case "Neha Sonu":
+			no=9148990135L;
+			System.out.println("Case 147");
+			break;
+			case "Neha Bestoo":
+			no=9880488195L;
+			System.out.println("Case 148");
+			break;
+			case "Megha":
+			no=7411427947L;
+			System.out.println("Case 149");
+			break;case "Maria Innis":
+			no=8310997741L;
+			System.out.println("Case 150");
+			break;
+			default :
+			System.out.println("No Match Found");
+			break;
+		}
+		return no;
+	}
+}
---
+++ methodsusingswitchcase/ContactsSearchExecutor.java
@@ -0,0 +1,305 @@
+class ContactsSearchExecutor
+{
+	public static void main(String[] cse){
+		long dramaqueen=Contacts.search("Drama Queen");
+		System.out.println(dramaqueen);//1
+		long daddy=Contacts.search("Daddy");
+		System.out.println(daddy);//2
+		long mumma=Contacts.search("Mumma");
+		System.out.println(mumma);//3
+		long srujana=Contacts.search("Srujana");
+		System.out.println(srujana);//4
+		long manju=Contacts.search("Manju");
+		System.out.println(manju);//5
+		long akshatha=Contacts.search("Akshatha");
+		System.out.println(akshatha);//6
+		long soumya=Contacts.search("Soumya");
+		System.out.println(soumya);//7
+		long vysh=Contacts.search("Vyshnavi");
+		System.out.println(vysh);//8
+		long hema=Contacts.search("Hema");
+		System.out.println(hema);//9
+		long sandya=Contacts.search("Sandya");
+		System.out.println(sandya);//10
+		long ganesh=Contacts.search("Ganesh");
+		System.out.println(ganesh);//11
+		long abhiram=Contacts.search("Abhiram");
+		System.out.println(abhiram);//12
+		long yamini=Contacts.search("Yamini");
+		System.out.println(yamini);//13
+		long spoorthij=Contacts.search("Spoorthi");
+		System.out.println(spoorthij);//14
+		long noor=Contacts.search("Noor");
+		System.out.println(noor);//15
+		long varsha=Contacts.search("Varsha");
+		System.out.println(varsha);//16
+		long aiman=Contacts.search("Aiman");
+		System.out.println(aiman);//17
+		long yashu=Contacts.search("Yashu");
+		System.out.println(yashu);//18
+		long uzma=Contacts.search("Uzma");
+		System.out.println(uzma);//19
+		long umera=Contacts.search("Umera");
+		System.out.println(umera);//20
+		long umme=Contacts.search("Umme Kulsum");
+		System.out.println(umme);//21
+		long teju=Contacts.search("Teju");
+		System.out.println(teju);//22
+		long tejaa=Contacts.search("Tejasvini");
+		System.out.println(tejaa);//23
+		long teja=Contacts.search("Tejaswini Bsc");
+		System.out.println(teja);//24
+		long tasmiya=Contacts.search("Tasmiya Jnes");
+		System.out.println(tasmiya);//25
+		long tasleem=Contacts.search("Tasleem Jnes");
+		System.out.println(tasleem);//26
+		long tanus=Contacts.search("Tanu Sav");
+		System.out.println(tanus);//27
+		long tanuja=Contacts.search("Tanuja Badima");
+		System.out.println(tanuja);//28
+		long tanu=Contacts.search("Tanu");
+		System.out.println(tanu);//29
+		long sweety=Contacts.search("Sweety");
+		System.out.println(sweety);//30
+		long sushmitha=Contacts.search("Sushmita");
+		System.out.println(sushmitha);//31
+		long surjith=Contacts.search("Surjith");
+		System.out.println(surjith);//32
+		long suresh=Contacts.search("Suresh");
+		System.out.println(suresh);//33
+		long sunithap=Contacts.search("Sunitha");
+		System.out.println(sunithap);//34
+		long sunitha=Contacts.search("Sunitha Mami");
+		System.out.println(sunitha);//35
+		long summitra=Contacts.search("Summitra");
+		System.out.println(summitra);//36
+		long summaya=Contacts.search("Summaya");
+		System.out.println(summaya);//37
+		long sujan=Contacts.search("Sujan");
+		System.out.println(sujan);//38
+		long suhana=Contacts.search("Suhana");
+		System.out.println(suhana);//39
+		long sudheer=Contacts.search("Sudheer");
+		System.out.println(sudheer);//40
+		long shreyas=Contacts.search("Shreyas");
+		System.out.println(shreyas);//41
+		long spoorthi=Contacts.search("Spoorthi Dvs");
+		System.out.println(spoorthi);//42
+		long spoo=Contacts.search("Spoo");
+		System.out.println(spoo);//43
+		long soumyas=Contacts.search("Soumya Sis");
+		System.out.println(soumyas);//44
+		long soumyar=Contacts.search("Soumya Raj Dvs");
+		System.out.println(soumyar);//45
+		long soudamini=Contacts.search("Soudamini");
+		System.out.println(soudamini);//46
+		long sonikad=Contacts.search("Sonika Dvs");
+		System.out.println(sonikad);//47
+		long sonika=Contacts.search("Sonika");
+		System.out.println(sonika);//48
+		long sneha=Contacts.search("Sneha");
+		System.out.println(sneha);//49
+		long snehad=Contacts.search("Sneha DVS");
+		System.out.println(snehad);//50
+		long sinchanas=Contacts.search("Sinchana S Dvs");
+		System.out.println(sinchanas);//51
+		long sinchana=Contacts.search("Sinchana R");
+		System.out.println(sinchana);//52
+		long sinchanab=Contacts.search("Sinchana Baggi Dvs");
+		System.out.println(sinchanab);//53
+		long shwetha=Contacts.search("Shwetha Akka Dvs");
+		System.out.println(shwetha);//54
+		long shubha=Contacts.search("Shubha");
+		System.out.println(shubha);//55
+		long shruthi=Contacts.search("Shruthi");
+		System.out.println(shruthi);//56
+		long shreyanka=Contacts.search("Shreyanka");
+		System.out.println(shreyanka);//57
+		long shivram=Contacts.search("Shivram Mousa");
+		System.out.println(shivram);//58
+		long shivani=Contacts.search("Shivani Dvs");
+		System.out.println(shivani);//59
+		long sharath=Contacts.search("Sharath Sir Placement Cell");
+		System.out.println(sharath);//60
+		long shalinij=Contacts.search("Shalini Jnes");
+		System.out.println(shalinij);//61
+		long shalinil=Contacts.search("Shalini L");
+		System.out.println(shalinil);//62
+		long shalini=Contacts.search("Shalini");
+		System.out.println(shalini);//63
+		long shali=Contacts.search("Shali L");
+		System.out.println(shali);//64
+		long shaila=Contacts.search("Shaila Bca");
+		System.out.println(shaila);//65
+		long savithri=Contacts.search("Savithri maam");
+		System.out.println(savithri);//66
+		long sarswathi=Contacts.search("Sarswathi Aunty");
+		System.out.println(sarswathi);//67
+		long saouda=Contacts.search("Saouda");
+		System.out.println(saouda);//68
+		long sanjur=Contacts.search("Sanju");
+		System.out.println(sanjur);//69
+		long sanjusonu=Contacts.search("Sanju Sonu Rajput");
+		System.out.println(sanjusonu);//70
+		long sanju=Contacts.search("Sanju Dvs");
+		System.out.println(sanju);//71
+		long sanjay=Contacts.search("Sanjay Dvs");
+		System.out.println(sanjay);//72
+		long sanjanasa=Contacts.search("Sanjana SA");
+		System.out.println(sanjanasa);//73
+		long sanjanar=Contacts.search("Sanjana R Juniour");
+		System.out.println(sanjanar);//74
+		long sanjanap=Contacts.search("Sanjana P");
+		System.out.println(sanjanap);//75
+		long sanjanam=Contacts.search("Sanjana M Prasad");
+		System.out.println(sanjanam);//76
+		long sanjanaka=Contacts.search("Sanjana Kadkol Jnnce");
+		System.out.println(sanjanaka);//77
+		long sanjanak=Contacts.search("Sanjana K");
+		System.out.println(sanjanak);//78
+		long sanjana=Contacts.search("Sanjana");
+		System.out.println(sanjana);//79
+		long saniyat=Contacts.search("Saniya Tasleem");
+		System.out.println(saniyat);//80
+		long saniya=Contacts.search("Saniya Aiman");
+		System.out.println(saniya);//81
+		long sangeetha=Contacts.search("Sangeetha");
+		System.out.println(sangeetha);//82
+		long sandeep=Contacts.search("Sandeep Mama Bdvt");
+		System.out.println(sandeep);//83
+		long samana=Contacts.search("Samana");
+		System.out.println(samana);//84
+		long sakshij=Contacts.search("Sakshi");
+		System.out.println(sakshij);//85
+		long sakshi=Contacts.search("Sakshi Dvs");
+		System.out.println(sakshi);//86
+		long sahanami=Contacts.search("Sahana Miss");
+		System.out.println(sahanami);//87
+		long sahanam=Contacts.search("Sahana Mam Sav");
+		System.out.println(sahanam);//88
+		long sahana=Contacts.search("Sahana Jnnce");
+		System.out.println(sahana);//89
+		long sadiya=Contacts.search("Sadiya Jnnce");
+		System.out.println(sadiya);//90
+		long ruchitha=Contacts.search("Ruchitha Jnnce");
+		System.out.println(ruchitha);//91
+		long roopas=Contacts.search("Roopa Senior Jnnce");
+		System.out.println(roopas);//92
+		long roopa=Contacts.search("Roopa Badimaa");
+		System.out.println(roopa);//93
+		long rekhap=Contacts.search("Rekha Puppu");
+		System.out.println(rekhap);//94
+		long rekha=Contacts.search("Rekha");
+		System.out.println(rekha);//95
+		long raziya=Contacts.search("Raziya Maam");
+		System.out.println(raziya);//96
+		long ratna=Contacts.search("Ratna Sonu");
+		System.out.println(ratna);//97
+		long rasmitha=Contacts.search("Rasmitha");
+		System.out.println(rasmitha);//98
+		long rashmi=Contacts.search("Rashmi Maam Dvs");
+		System.out.println(rashmi);//99
+		long rashib=Contacts.search("Rashi Beauty Parlour");
+		System.out.println(rashib);//100
+		long ranjithaj=Contacts.search("Ranjitha Jnnce");
+		System.out.println(ranjithaj);//101
+		long ranjitha=Contacts.search("Ranjitha Akka");
+		System.out.println(ranjitha);//102
+		long ramyas=Contacts.search("Ramya Sonu Sav");
+		System.out.println(ramyas);//103
+		long ramya=Contacts.search("Ramya Jnnce");
+		System.out.println(ramya);//104
+		long rakshitham=Contacts.search("Rakshitha Mam");
+		System.out.println(rakshitham);//105
+		long rakshitha=Contacts.search("Rakshitha Jnnce");
+		System.out.println(rakshitha);//106
+		long rajaf=Contacts.search("Raja Fashions");
+		System.out.println(rajaf);//107
+		long rahulj=Contacts.search("Rahul NK jnnce");
+		System.out.println(rahulj);//108
+		long rahuld=Contacts.search("Rahul NK Dvs");
+		System.out.println(rahuld);//109
+		long raghu=Contacts.search("Raghu Mama");
+		System.out.println(raghu);//110
+		long raghavi=Contacts.search("Raghavi");
+		System.out.println(raghavi);//111
+		long ragvendras=Contacts.search("Ragvendra Sir Jnnce");
+		System.out.println(ragvendras);//112
+		long ragvendra=Contacts.search("Ragvendra Dvs");
+		System.out.println(ragvendra);//113
+		long punithk=Contacts.search("Punith Kademane");
+		System.out.println(punithk);//114
+		long punith=Contacts.search("Punith Dvs");
+		System.out.println(punith);//115
+		long priyanka=Contacts.search("Priyanka Maam");
+		System.out.println(priyanka);//116
+		long priyas=Contacts.search("Priya Sonu");
+		System.out.println(priyas);//117
+		long priyad=Contacts.search("Priya Dvs");
+		System.out.println(priyad);//118
+		long prarthanaj=Contacts.search("Prarthana Jnnce");
+		System.out.println(prarthanaj);//119
+		long prarthana=Contacts.search("Prarthana");
+		System.out.println(prarthana);//120
+		long prajwals=Contacts.search("Prajwal Singh Bro");
+		System.out.println(prajwals);//121
+		long prajwal=Contacts.search("Prajwal Bhaya");
+		System.out.println(prajwal);//122
+		long pragathi=Contacts.search("Pragathi Jnnce");
+		System.out.println(pragathi);//123
+		long prabha=Contacts.search("Prabha Bhatt Mam");
+		System.out.println(prabha);//124
+		long powrnami=Contacts.search("Powrnami Sonu frnd");
+		System.out.println(powrnami);//125
+		long poorvij=Contacts.search("Poorvitha");
+		System.out.println(poorvij);//126
+		long poorvi=Contacts.search("Poorvi");
+		System.out.println(poorvi);//127
+		long poojajs=Contacts.search("Pooja");
+		System.out.println(poojajs);//128
+		long poojaj=Contacts.search("Pooja Jnnce");
+		System.out.println(poojaj);//129
+		long pavan=Contacts.search("Pavan");
+		System.out.println(pavan);//130
+		long pari=Contacts.search("Pari");
+		System.out.println(pari);//131
+		long pappa=Contacts.search("Pappa");
+		System.out.println(pappa);//132
+		long dad=Contacts.search("Dad");
+		System.out.println(dad);//133
+		long panendra=Contacts.search("Panendra Sir");
+		System.out.println(panendra);//134
+		long padma=Contacts.search("Padma Aunty");
+		System.out.println(padma);//135
+		long noothan=Contacts.search("Noothan");
+		System.out.println(noothan);//136
+		long noorainb=Contacts.search("Noorain Bro");
+		System.out.println(noorainb);//137
+		long noorain=Contacts.search("Noorain");
+		System.out.println(noorain);//138
+		long noorm=Contacts.search("Noor Mom");
+		System.out.println(noorm); //139
+		long niveditha=Contacts.search("Niveditha Dvs");
+		System.out.println(niveditha);//140
+		long nirmala=Contacts.search("Nirmala Mousi");
+		System.out.println(nirmala);//141
+		long nikkath=Contacts.search("Nikkath Maam");
+		System.out.println(nikkath);//142
+		long nikitham=Contacts.search("Nikitha Mam");
+		System.out.println(nikitham);//143
+		long nikithad=Contacts.search("Nikitha");
+		System.out.println(nikithad);//144
+		long nethravathi=Contacts.search("Nethravathi Mam");
+		System.out.println(nethravathi);//145
+		long neha=Contacts.search("Neha");
+		System.out.println(neha);//146
+		long nehas=Contacts.search("Neha Sonu");
+		System.out.println(nehas);//147
+		long nehab=Contacts.search("Neha Bestoo");
+		System.out.println(nehab);//148
+		long megha=Contacts.search("Megha");
+		System.out.println(megha);//149
+		long maria=Contacts.search("Maria Innis");
+		System.out.println(maria);//150
+	}
+}
