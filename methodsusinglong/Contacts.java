f7cb1192ad4d189b003a8302c7b5dabe7dc1b38f
Chandana Omkarappa <chandanaomkarappa@gmail.com>
2026-04-15 18:10:38 +0530

    methodsusinglong examples

---
+++ methodsusinglong/Contacts.java
@@ -0,0 +1,313 @@
+class Contacts
+{
+	static long searchContactsNumber(String contactName)
+	{
+		System.out.println("Search Contacts Method Invoked");
+		if(contactName == "Drama Queen"){
+			return 6364212629L;//1
+		}else if(contactName == "Daddy"){
+			return 9980051095L;//2
+		}else if(contactName == "Mumma"){
+			return 6364890106L;//3
+		}else if(contactName == "Srujana Jnnce"){
+			return 7892182192L;//4
+		}else if(contactName == "Manju"){
+			return 9008547568L;//5
+		}else if(contactName == "Akshatha"){
+			return 9108260528L;//6
+		}else if(contactName == "Soumya"){
+			return 6360438274L;//7
+		}else if(contactName == "Vyshnavi Jnnce"){
+			return 9980801478L;//8
+		}else if(contactName == "Hema"){
+			return 7676420983L;//9
+		}else if(contactName == "Sandya Jnnce"){
+			return 9611588479L;//10
+		}else if(contactName == "Ganesh Dvs"){
+			return 8951536148L;//11
+		}else if(contactName == "Abhiram Dvs"){
+			return 9483296548L;//12
+		}else if(contactName == "Yamini Jnnce"){
+			return 6364210640L;//13
+		}
+		else if(contactName == "Spoorti Jnnce"){
+			return 9108204232L;//14
+		}
+		else if(contactName == "Noor Dvs"){
+			return 8050998870L;//15
+		}else if(contactName == "Varsha Jnnce"){
+			return 953585775L;//16
+		}else if(contactName == "Aiman"){
+			return 9743146885L;//17
+		}else if(contactName == "Yashu Jnnce"){
+			return 6361980460L;//18
+		}else if(contactName == "Uzma Jnnce"){
+				return 8904220115L;//19
+		}else if(contactName == "Umera Sav"){
+			return 9380800425L;//20
+		}else if(contactName == "Umme Kulsum Sav"){
+			return 8747088978L;//21
+		}else if(contactName == "Teju gowda"){
+			return 9663780699L;//22
+		}else if(contactName == "Tejasvini Jnnce"){
+			return 9483360461L;//23
+		}else if(contactName == "Tejaswini Bsc Dvs"){
+			return 9353491161L;//24
+		}else if(contactName == "Tasmiya Jnes"){
+			return 9380054155L;//25
+		}else if(contactName == "Tasleem Jnes"){
+			return 9663325736L;//26
+		}else if(contactName == "Tanuja Badima"){
+			return 9980428152L;//27
+		}else if(contactName == "Tanu Sav"){
+			return 9113613271L;//28
+		}else if(contactName == "Tanu"){
+			return 9980428152L;//29
+		}else if(contactName == "Sweety"){
+			return 7204835031L;//30
+		}else if(contactName == "Sushmita Sav Sonu"){
+			return 9845451531L;//31
+		}else if(contactName == "Surjith Singh"){
+			return 8217827798L;//32
+		}else if(contactName == "Suresh Sir Jnnce"){
+			return 9986502357L;//33
+		}else if(contactName == "Sunitha Puppu"){
+			return 7676495404L;//34
+		}else if(contactName == "Sunitha Mami"){
+			return 9108744691L;//35
+		}else if(contactName == "Summitra Jnnce"){
+			return 7795796174L;//36
+		}else if(contactName == "Summaya Jnnce"){
+			return 8217416902L;//37
+		}else if(contactName == "Sujan Singh"){
+			return 9481174624L;//38
+		}else if(contactName == "Suhana Khan"){
+			return 6364084711L;//39
+		}else if(contactName == "Sudheer sir Jnnce"){
+			return 9611128352L;//40
+		}else if(contactName == "Shreyas Studio"){
+			return 9880061026L;//41
+		}else if(contactName == "Spoorthi Dvs"){
+			return 7338519184L;//42
+		}else if(contactName == "Spoo Jnnce"){
+			return 9972378014L;//43
+		}else if(contactName == "Soumya Sis"){
+			return 6361162596L;//44
+		}else if(contactName ==  "Soumya Raj Dvs"){
+			return 9945384935L;//45
+		}else if(contactName == "Soudamini"){
+			return 7411407058L;//46
+		}else if(contactName == "Sonika Dvs"){
+			return 6363741489L;//47
+		}else if(contactName == "Sonika"){
+			return 9844281235L;//48
+		}else if(contactName == "Sneha Jnnce"){
+			return 8310857027L;//49
+		}else if(contactName == "Sneha DVS"){
+			return 8277606800L;//50
+		}else if(contactName == "Sinchana S Dvs"){
+			return 8660943289L;//51
+		}else if(contactName == "Sinchana R"){
+			return 8618316689L;//52
+		}else if(contactName == "Sinchana Baggi Dvs"){
+			return 9071840010L;//53
+		}else if(contactName == "Shwetha Akka Dvs"){
+			return 7259872154L;//54
+		}else if(contactName == "Shubha"){
+			return 9980451820L;//55
+		}else if(contactName == "Shruthi Jnnce"){
+			return 7019437293L;//56
+		}else if(contactName == "Shreyanka Maam"){
+			return 9110630018L;//57
+		}else if(contactName == "Shivram Mousa"){
+			return 9845222898L;//58
+		}else if(contactName == "Shivani Dvs"){
+			return 9019653896L;//59
+		}else if(contactName == "Sharath Sir Placement Cell"){
+			return 9060909027L;//60
+		}else if(contactName == "Shalini Jnes"){
+			return 8073697756L;//61
+		}else if(contactName == "Shalini L"){
+			return 7349147908L;//62
+		}else if(contactName == "Shalini"){
+			return 7760045206L;//63
+		}else if(contactName == "Shali L"){
+			return 9110494163L;//64
+		}else if(contactName == "Shaila Bca"){
+			return 7676092184L;//65
+		}else if(contactName == "Savithri maam"){
+			return 9945154081L;//66
+		}else if(contactName == "Sarswathi Aunty"){
+			return 9945557709L;//67
+		}else if(contactName == "Saouda"){
+			return 9606423787L;//68
+		}else if(contactName == "Sanju"){
+			return 6364768033L;//69
+		}else if(contactName == "Sanju Sonu Rajput"){
+			return 9740174891L;//70
+		}else if(contactName == "Sanju Dvs"){
+			return 7795300865L;//71
+		}else if(contactName == "Sanjay Dvs"){
+			return 9900972761L;//72
+		}else if(contactName == "Sanjana SA"){
+			return 7892031681L;//73
+		}else if(contactName == "Sanjana R Juniour"){
+			return 8618693101L;//74
+		}else if(contactName == "Sanjana P"){
+			return 9008060513L;//75
+		}else if(contactName == "Sanjana M Prasad"){
+			return 9164904610L;//76
+		}else if(contactName == "Sanjana Kadkol Jnnce"){
+			return 7204171032L;//77
+		}else if(contactName == "Sanjana K"){
+			return 77905067306L;//78
+		}else if(contactName  == "Sanjana"){
+			return 9353112171L;//79
+		}else if(contactName == "Saniya Tasleem"){
+			return 9986526307L;//80
+		}else if(contactName == "Saniya Aiman"){
+			return 7411878513L;//81
+		}else if(contactName == "Sangeetha Dvs"){
+			return 7022899641L;//82
+		}else if(contactName == "Sandeep Mama Bdvt"){
+			return 8095409817L;//83
+		}else if(contactName == "Samana Jnnce"){
+			return 8277044763L;//84
+		}else if(contactName == "Sakshi Jnnce"){
+			return 9740451593L;//85
+		}else if(contactName == "Sakshi Dvs"){
+			return 8050595630L;//86
+		}else if(contactName == "Sahana Miss"){
+			return 8867856709L;//87
+		}else if(contactName == "Sahana Mam Sav"){
+			return 9380622268L;//88
+		}else if(contactName == "Sahana Jnnce"){
+			return 8050931885L;//89
+		}else if(contactName == "Sadiya Jnnce"){
+			return 7760497556L;//90
+		}else if(contactName == "Ruchitha Jnnce"){
+			return 7022392944L;//91
+		}else if(contactName == "Roopa Senior Jnnce"){
+			return 6362842374L;//92
+		}else if(contactName == "Roopa Badimaa"){
+			return 9844970623L;//93
+		}else if(contactName == "Rekha Puppu"){
+			return 9035752815L;//94
+		}else if(contactName == "Rekha"){
+			return 9482153785L;//95
+		}else if(contactName == "Raziya Maam"){
+			return 9108364432L;//96
+		}else if(contactName == "Ratna Sonu"){
+			return 8217601875L;//97
+		}else if(contactName == "Rasmitha"){
+			return 9606230067L;//98
+		}else if(contactName == "Rashmi Maam Dvs"){
+			return 9535461223L;//99
+		}else if(contactName == "Rashi Beauty Parlour"){
+			return 9880038969L;//100
+		}else if(contactName == "Ranjitha Jnnce"){
+			return 6360179043L;//101
+		}else if(contactName == "Ranjitha Akka"){
+			return 9611892923L;//102
+		}else if(contactName == "Ramya Sonu Sav"){
+			return 9743346064L;//103
+		}else if(contactName == "Ramya Jnnce"){
+			return 9900820828L;//104
+		}else if(contactName == "Rakshitha Mam"){
+			return 9738865181L;//105
+		}else if(contactName == "Rakshitha Jnnce"){
+			return 7337600732L;//106
+		}else if(contactName == "Raja Fashions"){
+			return 9880086565L;//107
+		}else if(contactName  == "Rahul NK jnnce"){
+			return 7975719907L;//108
+		}else if(contactName ==  "Rahul NK Dvs"){
+			return 8088163662L;//109
+		}else if(contactName == "Raghu Mama"){
+			return 9482207180L;//110
+		}else if(contactName == "Raghavi"){
+			return 8073957626L;//111
+		}else if(contactName == "Ragvendra Sir Jnnce"){
+			return 7019095137L;//112
+		}else if(contactName == "Ragvendra Dvs"){
+			return 8867144372L;//113
+		}else if(contactName == "Punith Kademane"){
+			return 9535167488L;//114
+		}else if(contactName == "Punith Dvs"){
+			return 9146446966L;//115
+		}else if(contactName == "Priyanka Maam"){
+			return 7204376031L;//116
+		}else if(contactName == "Priya Sonu"){
+			return 8971796946L;//117
+		}else if(contactName == "Priya Dvs"){
+			return 6364771445L;//118
+		}else if(contactName == "Prarthana Jnnce"){
+			return 8147557161L;//119
+		}else if(contactName == "Prarthana"){
+			return 9740968993L;//120
+		}else if(contactName == "Prajwal Singh Bro"){
+			return 9480206877L;//121
+		}else if(contactName == "Prajwal Bhaya"){
+			return 6364352057L;//122
+		}else if(contactName == "Pragathi Jnnce"){
+			return 6364781971L;//123
+		}else if(contactName == "Prabha Bhatt Mam"){
+			return 9036093038L;//124
+		}else if(contactName == "Powrnami Sonu frnd"){
+			return 7618757542L;//125
+		}else if(contactName == "Poorvitha Jnnce"){
+			return 6362400256L;//126
+		}else if(contactName == "Poorvi"){
+			return 9632989439L;//127
+		}else if(contactName == "Pooja Jnnce shimoga"){
+			return 9686599159L;//128
+		}else if(contactName == "Pooja Jnnce"){
+			return 7259041363L;//129
+		}else if(contactName == "Pavan"){
+			return 6360015825L;//130
+		}else if(contactName == "Pari Singh"){
+			return 7019639980L;//131
+		}else if(contactName == "Pappa"){
+			return 7483574575L;//132
+		}else if(contactName == "Dad"){
+			return 9902281627L;//133
+		}else if(contactName == "Panendra Sir College"){
+			return 8867261635L;//134
+		}else if(contactName == "Padma Aunty"){
+			return 9632075069L;//135
+		}else if(contactName == "Noothan KP"){
+			return 8073935287L;//136
+		}else if(contactName == "Noorain Bro"){
+			return 9148339088L;//137
+		}else if(contactName == "Noorain"){
+			return 9535385050L; //138
+		}else if(contactName == "Noor Mom"){
+			return 6364736563L; //139
+		}else if(contactName == "Niveditha Dvs"){
+			return 7795142663L;//140
+		}else if(contactName == "Nirmala Mousi"){
+			return 9880250476L;//141
+		}else if(contactName == "Nikkath Maam"){
+			return 9632727738L;//142
+		}else if(contactName == "Nikitha Mam"){
+			return 9741294819L;//143
+		}else if(contactName == "Nikitha Dvs"){
+			return 8310758972L;//144
+		}else if(contactName == "Nethravathi Mam"){
+			return 9241223948L;//145
+		}else if(contactName ==  "Neha"){
+			return 9916266836L;//146
+		}else if(contactName == "Neha Sonu"){
+			return 9148990135L;//147
+		}else if(contactName == "Neha Bestoo"){
+			return 9880488195L;//148
+		}else if(contactName == "Megha Senior Jnnce"){
+			return 7411427947L;//149
+		}else if(contactName == "Maria Innis"){
+			return 8310997741L;//150
+		}else{
+			System.out.println("Contacts Not Found");
+		}
+		return 0;
+	}
+}
---
+++ methodsusinglong/ContactsExecutor.java
@@ -0,0 +1,306 @@
+class ContactsExecutor
+{
+	public static void main(String[] ce){
+		long dramaqueen=Contacts.searchContactsNumber("Drama Queen");
+		System.out.println(dramaqueen);//1
+		long daddy=Contacts.searchContactsNumber("Daddy");
+		System.out.println(daddy);//2
+		long mumma=Contacts.searchContactsNumber("Mumma");
+		System.out.println(mumma);//3
+		long srujana=Contacts.searchContactsNumber("Srujana Jnnce");
+		System.out.println(srujana);//4
+		long manju=Contacts.searchContactsNumber("Manju");
+		System.out.println(manju);//5
+		long akshatha=Contacts.searchContactsNumber("Akshatha");
+		System.out.println(akshatha);//6
+		long soumya=Contacts.searchContactsNumber("Soumya");
+		System.out.println(soumya);//7
+		long vysh=Contacts.searchContactsNumber("Vyshnavi Jnnce");
+		System.out.println(vysh);//8
+		long hema=Contacts.searchContactsNumber("Hema");
+		System.out.println(hema);//9
+		long sandya=Contacts.searchContactsNumber("Sandya Jnnce");
+		System.out.println(sandya);//10
+		long ganesh=Contacts.searchContactsNumber("Ganesh Dvs");
+		System.out.println(ganesh);//11
+		long abhiram=Contacts.searchContactsNumber("Abhiram Dvs");
+		System.out.println(abhiram);//12
+		long yamini=Contacts.searchContactsNumber("Yamini Jnnce");
+		System.out.println(yamini);//13
+		long spoorthij=Contacts.searchContactsNumber("Spoorti Jnnce");
+		System.out.println(spoorthij);//14
+		long noor=Contacts.searchContactsNumber("Noor Dvs");
+		System.out.println(noor);//15
+		long varsha=Contacts.searchContactsNumber("Varsha Jnnce");
+		System.out.println(varsha);//16
+		long aiman=Contacts.searchContactsNumber("Aiman");
+		System.out.println(aiman);//17
+		long yashu=Contacts.searchContactsNumber("Yashu Jnnce");
+		System.out.println(yashu);//18
+		long uzma=Contacts.searchContactsNumber("Uzma Jnnce");
+		System.out.println(uzma);//19
+		long umera=Contacts.searchContactsNumber("Umera Sav");
+		System.out.println(umera);//20
+		long umme=Contacts.searchContactsNumber("Umme Kulsum Sav");
+		System.out.println(umme);//21
+		long teju=Contacts.searchContactsNumber("Teju gowda");
+		System.out.println(teju);//22
+		long tejaa=Contacts.searchContactsNumber("Tejasvini Jnnce");
+		System.out.println(tejaa);//23
+		long teja=Contacts.searchContactsNumber("Tejaswini Bsc Dvs");
+		System.out.println(teja);//24
+		long tasmiya=Contacts.searchContactsNumber("Tasmiya Jnes");
+		System.out.println(tasmiya);//25
+		long tasleem=Contacts.searchContactsNumber("Tasleem Jnes");
+		System.out.println(tasleem);//26
+		long tanus=Contacts.searchContactsNumber("Tanu Sav");
+		System.out.println(tanus);//27
+		long tanuja=Contacts.searchContactsNumber("Tanuja Badima");
+		System.out.println(tanuja);//28
+		long tanu=Contacts.searchContactsNumber("Tanu");
+		System.out.println(tanu);//29
+		long sweety=Contacts.searchContactsNumber("Sweety");
+		System.out.println(sweety);//30
+		long sushmitha=Contacts.searchContactsNumber("Sushmita Sav Sonu");
+		System.out.println(sushmitha);//31
+		long surjith=Contacts.searchContactsNumber("Surjith Singh");
+		System.out.println(surjith);//32
+		long suresh=Contacts.searchContactsNumber("Suresh Sir Jnnce");
+		System.out.println(suresh);//33
+		long sunithap=Contacts.searchContactsNumber("Sunitha Puppu");
+		System.out.println(sunithap);//34
+		long sunitha=Contacts.searchContactsNumber("Sunitha Mami");
+		System.out.println(sunitha);//35
+		long summitra=Contacts.searchContactsNumber("Summitra Jnnce");
+		System.out.println(summitra);//36
+		long summaya=Contacts.searchContactsNumber("Summaya Jnnce");
+		System.out.println(summaya);//37
+		long sujan=Contacts.searchContactsNumber("Sujan Singh");
+		System.out.println(sujan);//38
+		long suhana=Contacts.searchContactsNumber("Suhana Khan");
+		System.out.println(suhana);//39
+		long sudheer=Contacts.searchContactsNumber("Sudheer sir Jnnce");
+		System.out.println(sudheer);//40
+		long shreyas=Contacts.searchContactsNumber("Shreyas Studio");
+		System.out.println(shreyas);//41
+		long spoorthi=Contacts.searchContactsNumber("Spoorthi Dvs");
+		System.out.println(spoorthi);//42
+		long spoo=Contacts.searchContactsNumber("Spoo Jnnce");
+		System.out.println(spoo);//43
+		long soumyas=Contacts.searchContactsNumber("Soumya Sis");
+		System.out.println(soumyas);//44
+		long soumyar=Contacts.searchContactsNumber("Soumya Raj Dvs");
+		System.out.println(soumyar);//45
+		long soudamini=Contacts.searchContactsNumber("Soudamini");
+		System.out.println(soudamini);//46
+		long sonikad=Contacts.searchContactsNumber("Sonika Dvs");
+		System.out.println(sonikad);//47
+		long sonika=Contacts.searchContactsNumber("Sonika");
+		System.out.println(sonika);//48
+		long sneha=Contacts.searchContactsNumber("Sneha Jnnce");
+		System.out.println(sneha);//49
+		long snehad=Contacts.searchContactsNumber("Sneha DVS");
+		System.out.println(snehad);//50
+		long sinchanas=Contacts.searchContactsNumber("Sinchana S Dvs");
+		System.out.println(sinchanas);//51
+		long sinchana=Contacts.searchContactsNumber("Sinchana R");
+		System.out.println(sinchana);//52
+		long sinchanab=Contacts.searchContactsNumber("Sinchana Baggi Dvs");
+		System.out.println(sinchanab);//53
+		long shwetha=Contacts.searchContactsNumber("Shwetha Akka Dvs");
+		System.out.println(shwetha);//54
+		long shubha=Contacts.searchContactsNumber("Shubha");
+		System.out.println(shubha);//55
+		long shruthi=Contacts.searchContactsNumber("Shruthi Jnnce");
+		System.out.println(shruthi);//56
+		long shreyanka=Contacts.searchContactsNumber("Shreyanka Maam");
+		System.out.println(shreyanka);//57
+		long shivram=Contacts.searchContactsNumber("Shivram Mousa");
+		System.out.println(shivram);//58
+		long shivani=Contacts.searchContactsNumber("Shivani Dvs");
+		System.out.println(shivani);//59
+		long sharath=Contacts.searchContactsNumber("Sharath Sir Placement Cell");
+		System.out.println(sharath);//60
+		long shalinij=Contacts.searchContactsNumber("Shalini Jnes");
+		System.out.println(shalinij);//61
+		long shalinil=Contacts.searchContactsNumber("Shalini L");
+		System.out.println(shalinil);//62
+		long shalini=Contacts.searchContactsNumber("Shalini");
+		System.out.println(shalini);//63
+		long shali=Contacts.searchContactsNumber("Shali L");
+		System.out.println(shali);//64
+		long shaila=Contacts.searchContactsNumber("Shaila Bca");
+		System.out.println(shaila);//65
+		long savithri=Contacts.searchContactsNumber("Savithri maam");
+		System.out.println(savithri);//66
+		long sarswathi=Contacts.searchContactsNumber("Sarswathi Aunty");
+		System.out.println(sarswathi);//67
+		long saouda=Contacts.searchContactsNumber("Saouda");
+		System.out.println(saouda);//68
+		long sanjur=Contacts.searchContactsNumber("Sanju");
+		System.out.println(sanjur);//69
+		long sanjusonu=Contacts.searchContactsNumber("Sanju Sonu Rajput");
+		System.out.println(sanjusonu);//70
+		long sanju=Contacts.searchContactsNumber("Sanju Dvs");
+		System.out.println(sanju);//71
+		long sanjay=Contacts.searchContactsNumber("Sanjay Dvs");
+		System.out.println(sanjay);//72
+		long sanjanasa=Contacts.searchContactsNumber("Sanjana SA");
+		System.out.println(sanjanasa);//73
+		long sanjanar=Contacts.searchContactsNumber("Sanjana R Juniour");
+		System.out.println(sanjanar);//74
+		long sanjanap=Contacts.searchContactsNumber("Sanjana P");
+		System.out.println(sanjanap);//75
+		long sanjanam=Contacts.searchContactsNumber("Sanjana M Prasad");
+		System.out.println(sanjanam);//76
+		long sanjanaka=Contacts.searchContactsNumber("Sanjana Kadkol Jnnce");
+		System.out.println(sanjanaka);//77
+		long sanjanak=Contacts.searchContactsNumber("Sanjana K");
+		System.out.println(sanjanak);//78
+		long sanjana=Contacts.searchContactsNumber("Sanjana");
+		System.out.println(sanjana);//79
+		long saniyat=Contacts.searchContactsNumber("Saniya Tasleem");
+		System.out.println(saniyat);//80
+		long saniya=Contacts.searchContactsNumber("Saniya Aiman");
+		System.out.println(saniya);//81
+		long sangeetha=Contacts.searchContactsNumber("Sangeetha Dvs");
+		System.out.println(sangeetha);//82
+		long sandeep=Contacts.searchContactsNumber("Sandeep Mama Bdvt");
+		System.out.println(sandeep);//83
+		long samana=Contacts.searchContactsNumber("Samana Jnnce");
+		System.out.println(samana);//84
+		long sakshij=Contacts.searchContactsNumber("Sakshi Jnnce");
+		System.out.println(sakshij);//85
+		long sakshi=Contacts.searchContactsNumber("Sakshi Dvs");
+		System.out.println(sakshi);//86
+		long sahanami=Contacts.searchContactsNumber("Sahana Miss");
+		System.out.println(sahanami);//87
+		long sahanam=Contacts.searchContactsNumber("Sahana Mam Sav");
+		System.out.println(sahanam);//88
+		long sahana=Contacts.searchContactsNumber("Sahana Jnnce");
+		System.out.println(sahana);//89
+		long sadiya=Contacts.searchContactsNumber("Sadiya Jnnce");
+		System.out.println(sadiya);//90
+		long ruchitha=Contacts.searchContactsNumber("Ruchitha Jnnce");
+		System.out.println(ruchitha);//91
+		long roopas=Contacts.searchContactsNumber("Roopa Senior Jnnce");
+		System.out.println(roopas);//92
+		long roopa=Contacts.searchContactsNumber("Roopa Badimaa");
+		System.out.println(roopa);//93
+		long rekhap=Contacts.searchContactsNumber("Rekha Puppu");
+		System.out.println(rekhap);//94
+		long rekha=Contacts.searchContactsNumber("Rekha");
+		System.out.println(rekha);//95
+		long raziya=Contacts.searchContactsNumber("Raziya Maam");
+		System.out.println(raziya);//96
+		long ratna=Contacts.searchContactsNumber("Ratna Sonu");
+		System.out.println(ratna);//97
+		long rasmitha=Contacts.searchContactsNumber("Rasmitha");
+		System.out.println(rasmitha);//98
+		long rashmi=Contacts.searchContactsNumber("Rashmi Maam Dvs");
+		System.out.println(rashmi);//99
+		long rashib=Contacts.searchContactsNumber("Rashi Beauty Parlour");
+		System.out.println(rashib);//100
+		long ranjithaj=Contacts.searchContactsNumber("Ranjitha Jnnce");
+		System.out.println(ranjithaj);//101
+		long ranjitha=Contacts.searchContactsNumber("Ranjitha Akka");
+		System.out.println(ranjitha);//102
+		long ramyas=Contacts.searchContactsNumber("Ramya Sonu Sav");
+		System.out.println(ramyas);//103
+		long ramya=Contacts.searchContactsNumber("Ramya Jnnce");
+		System.out.println(ramya);//104
+		long rakshitham=Contacts.searchContactsNumber("Rakshitha Mam");
+		System.out.println(rakshitham);//105
+		long rakshitha=Contacts.searchContactsNumber("Rakshitha Jnnce");
+		System.out.println(rakshitha);//106
+		long rajaf=Contacts.searchContactsNumber("Raja Fashions");
+		System.out.println(rajaf);//107
+		long rahulj=Contacts.searchContactsNumber("Rahul NK jnnce");
+		System.out.println(rahulj);//108
+		long rahuld=Contacts.searchContactsNumber("Rahul NK Dvs");
+		System.out.println(rahuld);//109
+		long raghu=Contacts.searchContactsNumber("Raghu Mama");
+		System.out.println(raghu);//110
+		long raghavi=Contacts.searchContactsNumber("Raghavi");
+		System.out.println(raghavi);//111
+		long ragvendras=Contacts.searchContactsNumber("Ragvendra Sir Jnnce");
+		System.out.println(ragvendras);//112
+		long ragvendra=Contacts.searchContactsNumber("Ragvendra Dvs");
+		System.out.println(ragvendra);//113
+		long punithk=Contacts.searchContactsNumber("Punith Kademane");
+		System.out.println(punithk);//114
+		long punith=Contacts.searchContactsNumber("Punith Dvs");
+		System.out.println(punith);//115
+		long priyanka=Contacts.searchContactsNumber("Priyanka Maam");
+		System.out.println(priyanka);//116
+		long priyas=Contacts.searchContactsNumber("Priya Sonu");
+		System.out.println(priyas);//117
+		long priyad=Contacts.searchContactsNumber("Priya Dvs");
+		System.out.println(priyad);//118
+		long prarthanaj=Contacts.searchContactsNumber("Prarthana Jnnce");
+		System.out.println(prarthanaj);//119
+		long prarthana=Contacts.searchContactsNumber("Prarthana");
+		System.out.println(prarthana);//120
+		long prajwals=Contacts.searchContactsNumber("Prajwal Singh Bro");
+		System.out.println(prajwals);//121
+		long prajwal=Contacts.searchContactsNumber("Prajwal Bhaya");
+		System.out.println(prajwal);//122
+		long pragathi=Contacts.searchContactsNumber("Pragathi Jnnce");
+		System.out.println(pragathi);//123
+		long prabha=Contacts.searchContactsNumber("Prabha Bhatt Mam");
+		System.out.println(prabha);//124
+		long powrnami=Contacts.searchContactsNumber("Powrnami Sonu frnd");
+		System.out.println(powrnami);//125
+		long poorvij=Contacts.searchContactsNumber("Poorvitha Jnnce");
+		System.out.println(poorvij);//126
+		long poorvi=Contacts.searchContactsNumber("Poorvi");
+		System.out.println(poorvi);//127
+		long poojajs=Contacts.searchContactsNumber("Pooja Jnnce shimoga");
+		System.out.println(poojajs);//128
+		long poojaj=Contacts.searchContactsNumber("Pooja Jnnce");
+		System.out.println(poojaj);//129
+		long pavan=Contacts.searchContactsNumber("Pavan");
+		System.out.println(pavan);//130
+		long pari=Contacts.searchContactsNumber("Pari Singh");
+		System.out.println(pari);//131
+		long pappa=Contacts.searchContactsNumber("Pappa");
+		System.out.println(pappa);//132
+		long dad=Contacts.searchContactsNumber("Dad");
+		System.out.println(dad);//133
+		long panendra=Contacts.searchContactsNumber("Panendra Sir College");
+		System.out.println(panendra);//134
+		long padma=Contacts.searchContactsNumber("Padma Aunty");
+		System.out.println(padma);//135
+		long noothan=Contacts.searchContactsNumber("Noothan KP");
+		System.out.println(noothan);//136
+		long noorainb=Contacts.searchContactsNumber("Noorain Bro");
+		System.out.println(noorainb);//137
+		long noorain=Contacts.searchContactsNumber("Noorain");
+		System.out.println(noorain);//138
+		long noorm=Contacts.searchContactsNumber("Noor Mom");
+		System.out.println(noorm); //139
+		long niveditha=Contacts.searchContactsNumber("Niveditha Dvs");
+		System.out.println(niveditha);//140
+		long nirmala=Contacts.searchContactsNumber("Nirmala Mousi");
+		System.out.println(nirmala);//141
+		long nikkath=Contacts.searchContactsNumber("Nikkath Maam");
+		System.out.println(nikkath);//142
+		long nikitham=Contacts.searchContactsNumber("Nikitha Mam");
+		System.out.println(nikitham);//143
+		long nikithad=Contacts.searchContactsNumber("Nikitha Dvs");
+		System.out.println(nikithad);//144
+		long nethravathi=Contacts.searchContactsNumber("Nethravathi Mam");
+		System.out.println(nethravathi);//145
+		long neha=Contacts.searchContactsNumber("Neha");
+		System.out.println(neha);//146
+		long nehas=Contacts.searchContactsNumber("Neha Sonu");
+		System.out.println(nehas);//147
+		long nehab=Contacts.searchContactsNumber("Neha Bestoo");
+		System.out.println(nehab);//148
+		long megha=Contacts.searchContactsNumber("Megha Senior Jnnce");
+		System.out.println(megha);//149
+		long maria=Contacts.searchContactsNumber("Maria Innis");
+		System.out.println(maria);//150
+			
+	}
+}
